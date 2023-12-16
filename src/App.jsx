88504692219e9a55600df5cf815bea3777455910
musab <musabdemiir@gmail.com>
2023-12-16 18:40:22 +0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,19 @@ import {
   TableHead,
   TableRow,
   Typography,
+  Box,
+  IconButton,
 } from '@mui/material';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import './App.css';
+import {
+  KeyboardDoubleArrowRightOutlined,
+  KeyboardDoubleArrowLeftOutlined,
+  KeyboardDoubleArrowUpOutlined,
+  ArrowDownward,
+} from '@mui/icons-material';
 
 function App() {
   const [position, setPosition] = useState(0);
@@ -38,11 +49,57 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
+  const getDirection = () => {
+    if (leftPwm > rightPwm) {
+      return 1;
+    } else if (rightPwm > leftPwm) {
+      return 2;
+    } else {
+      return 0;
+    }
+  };
+
+  const renderDirectionIcon = () => {
+    const direction = getDirection();
+
+    switch (direction) {
+      case 0:
+        return <KeyboardDoubleArrowUpOutlined style={{ fontSize: '5rem'}}  />;
+      case 1:
+        return <KeyboardDoubleArrowRightOutlined style={{ fontSize: '5rem'}} />;
+      case 2:
+        return <KeyboardDoubleArrowLeftOutlined  style={{ fontSize: '5rem'}} />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="App">
-      <Paper elevation={3} style={{ padding: '20px', width: '600px', textAlign: 'center' }}>
+      <Box mt={2}>
+        <Typography variant="h4">QTR ROBOT</Typography>
+        <IconButton color="primary" size="large">
+          {renderDirectionIcon()}
+        </IconButton>
+      </Box>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+        {sensorData.map((status, index) => (
+          <div
+            key={index}
+            style={{
+              width: '30px',
+              height: '30px',
+              border: '3px solid #D5BF86',
+              margin: '0 5px',
+              backgroundColor: status ? 'black' : 'white',
+            }}
+          ></div>
+        ))}
+      </div>
+
+      <Paper elevation={3} style={{ padding: '20px', width: '600px', textAlign: 'center', marginTop: '20px' }}>
         <Typography variant="h4" gutterBottom>
-          Gelişmiş Panel
         </Typography>
         <TableContainer className="TableContainer" component={Paper}>
           <Table>
@@ -62,20 +119,6 @@ function App() {
             </TableBody>
           </Table>
         </TableContainer>
-        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-          {sensorData.map((status, index) => (
-            <div
-              key={index}
-              style={{
-                width: '30px',
-                height: '30px',
-                border: '3px solid #D5BF86',
-                margin: '0 5px',
-                backgroundColor: status ? 'black' : 'white',
-              }}
-            ></div>
-          ))}
-        </div>
       </Paper>
     </div>
   );
